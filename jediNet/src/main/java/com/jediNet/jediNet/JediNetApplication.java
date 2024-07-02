c7d0e7fca68dfe1d2f329f99557ddd7ebc2c7862
junyeongjang <junyeong.dev@gmail.com>
2024-07-02 23:08:01 +0900

    init spring

---
+++ jediNet/src/main/java/com/jediNet/jediNet/JediNetApplication.java
@@ -0,0 +1,13 @@
+package com.jediNet.jediNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JediNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JediNetApplication.class, args);
+	}
+
+}
---
+++ jediNet/src/test/java/com/jediNet/jediNet/JediNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jediNet.jediNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JediNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
